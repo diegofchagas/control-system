1d9e4e3e3f4565b23212ff21b861bfd049aa012c
Diego Chagas <diegofchagas@gmail.com>
2024-03-20 16:51:25 -0300

    add datas para consulta de vendas de acordo com a data informada pelo usuario no context

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -1,29 +1,57 @@
-import { PropsWithChildren, createContext } from 'react'
-import { useFetch } from '../Hooks/useFetch'
+import { PropsWithChildren, createContext, useState } from "react";
+import { useFetch } from "../Hooks/useFetch";
 
-type DataContextProps ={
+type DataContextProps = {
   data: DataSalesProps[] | null;
-  loading:boolean;
-  error: string | null
-}
+  loading: boolean;
+  error: string | null;
+  inicio: string;
+  final: string;
+  changeStartDate: (date: string) => void;
+  changeEndDate: (date: string) => void;
+};
 
-type DataSalesProps ={
-  id:number;
-  nome:string;
-  preco:number;
-  status:string;
-  pagamento:string;
+type DataSalesProps = {
+  id: number;
+  nome: string;
+  preco: number;
+  status: "pago" | "processando" | "falha";
+  pagamento: "boleto" | "pix" | "cartao";
   parcelas: number | null;
   data: string;
-}
+};
 
-export const DataContext = createContext({ } as DataContextProps)
+export const DataContext = createContext({} as DataContextProps);
 
-export const DataContextProvider = ({children}: PropsWithChildren) => {
-   const {data,loading,error} = useFetch<DataSalesProps[]>('https://data.origamid.dev/vendas/')
+export const DataContextProvider = ({ children }: PropsWithChildren) => {
+  const [inicio, setInicio] = useState("");
+  const [final, setFinal] = useState("");
+
+  const { data, loading, error } = useFetch<DataSalesProps[]>(
+    `https://data.origamid.dev/vendas/?inicio=${inicio}&final=${final}`
+  );
+
+  const changeStartDate = (date: string) => {
+    setInicio(date);
+  };
+
+  const changeEndDate = (date: string) => {
+    setFinal(date);
+  };
+
   return (
-    <DataContext.Provider value={{data, loading,error}}>
-      {children}
+    <DataContext.Provider
+      value={{
+        data,
+        loading,
+        error,
+        inicio,
+        final,
+        changeStartDate,
+        changeEndDate,
+      }}
+    >
+      {children},
     </DataContext.Provider>
-  )
-}
+  );
+};

97891e91d86b11b9a1dc7f1f5d090b03f38e00dd
Diego Chagas <diegofchagas@gmail.com>
2024-03-22 12:02:09 -0300

    alterando estrutura do summary , informacoes sobre o resumo das vendas foram adicionadas podendo ser buscadas por meses de acordo com a escolha do cliente

--- src/pages/Summary/index.tsx
+++ src/pages/Summary/index.tsx
@@ -1,7 +1,60 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { DataContext } from "../../context/DataContext";
+import { SalesCharts } from "../../Components/SalesCharts";
 
 export const Summary = () => {
+  const { data } = useContext(DataContext);
   return (
-    <div>Summary</div>
-  )
-}
+    <section>
+      <div>
+        <div className="vendas">
+          <h2>Vendas</h2>
+          <span>
+            {data
+              ?.reduce((acc, total) => {
+                return acc + total.preco;
+              }, 0)
+              .toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}
+          </span>
+        </div>
+
+        <div className="recebido">
+          <h2>Recebido</h2>
+          <span>
+            {data
+              ?.filter((item) => item.status === "pago")
+              .reduce((acc, total) => {
+                return acc + total.preco;
+              }, 0)
+              .toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}
+          </span>
+        </div>
+
+        <div className="processando">
+          <h2>Recebido</h2>
+          <span>
+            {data
+              ?.filter((item) => item.status === "processando")
+              .reduce((acc, total) => {
+                return acc + total.preco;
+              }, 0)
+              .toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}
+          </span>
+        </div>
+      </div>
+      <div>
+        <h2>Gráfico de Vendas</h2>
+       {data && <SalesCharts data={data}/> } 
+      </div>
+    </section>
+  );
+};

f53004e15b71a1c7b9529cefc47bb21eb83afaa8
Diego Chagas <diegofchagas@gmail.com>
2024-03-22 11:59:15 -0300

    alterando estrutura do sidenav incluindo icons e informacoes sobre o site

--- src/Components/Sidenav/index.tsx
+++ src/Components/Sidenav/index.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { House, Storefront } from "@phosphor-icons/react";
+import { NavLink } from "react-router-dom";
+
 
 export const Sidenav = () => {
   return (
-    <div>Sidenav</div>
-  )
-}
+    <div>
+      <h1>Control System</h1>
+      <nav>
+        <ul>
+          <li>
+            <span>
+              {" "}
+              <House />{" "}
+            </span>
+            <NavLink to="/">Resumo</NavLink>
+          </li>
+          <li>
+            <span>
+              {" "}
+              <Storefront />{" "}
+            </span>
+            <NavLink to="/sales">Vendas</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};

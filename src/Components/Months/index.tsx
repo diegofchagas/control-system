f805b0f706c9c076e013a29050152750ad26c38d
Diego Chagas <diegofchagas@gmail.com>
2024-03-24 13:07:13 -0300

    add mais um mes ao componente Months

--- src/Components/Months/index.tsx
+++ src/Components/Months/index.tsx
@@ -7,6 +7,7 @@ export const Months = () => {
 
   return (
     <div style={{display:'flex', gap:'1rem'}}>
+      <MontButton month={-3}/>
       <MontButton month={-2}/>
       <MontButton month={-1}/>
       <MontButton month={0}/>

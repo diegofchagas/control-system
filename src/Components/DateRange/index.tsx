0a3da9eda6f622bfb4c1d1fd5539058c4b30d708
Diego Chagas <diegofchagas@gmail.com>
2024-03-24 13:07:51 -0300

    add novos estilos ao componente DateRange

--- src/Components/DateRange/index.tsx
+++ src/Components/DateRange/index.tsx
@@ -1,15 +1,16 @@
 import React, { useContext } from 'react'
 import { Input } from '../Input'
 import { DataContext } from '../../context/DataContext'
+import { Form } from './style';
 
 export const DateRange = () => {
 
   const {inicio,final, changeStartDate,changeEndDate} = useContext(DataContext);
 
   return (
-    <form>
+    <Form>
       <Input label='Início' value={inicio} onChange={({target}) =>changeStartDate(target.value)}/>
       <Input label='Final' value={final} onChange={({target})=> changeEndDate(target.value)}/>
-    </form>
+    </Form>
   )
 }
---
+++ src/Components/DateRange/style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+  max-width:31rem;
+  background-color: #FFF;
+  padding:1rem;
+  border-radius:16px;
+  display:flex;
+  justify-content:center;
+  gap:1rem;
+  margin-bottom:1rem;
+`;

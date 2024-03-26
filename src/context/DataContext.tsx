3725440e0277724795c1be168ecd4bc0cd34c404
Diego Chagas <diegofchagas@gmail.com>
2024-03-26 15:51:39 -0300

    add dias iniciais e finais no Context

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -1,4 +1,5 @@
 import { PropsWithChildren, createContext, useState } from "react";
+import { format, subDays } from 'date-fns';
 import { useFetch } from "../Hooks/useFetch";
 
 export type DataContextProps = {
@@ -25,14 +26,21 @@ export type DataSalesProps = {
 
 export const DataContext = createContext({} as DataContextProps);
 
+const getDaysAgo = (days: number)=> {
+  const date = subDays(new Date(), days);
+  return format(date, 'yyyy-MM-dd');
+}
+
 export const DataContextProvider = ({ children }: PropsWithChildren) => {
-  const [inicio, setInicio] = useState("");
-  const [final, setFinal] = useState("");
+  const [inicio, setInicio] = useState(getDaysAgo(20));
+  const [final, setFinal] = useState(getDaysAgo(0));
   const [linkActive, setLinkActive] = useState("")
 
   const { data, loading, error } = useFetch<DataSalesProps[]>(
     `https://data.origamid.dev/vendas/?inicio=${inicio}&final=${final}`
   );
+  
+
 
   const changeStartDate = (date: string) => {
     setInicio(date);
